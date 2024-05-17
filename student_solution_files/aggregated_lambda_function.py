9ccda6d3497b59d7ba2831e685b0f3fbb7b20e99
EDDIE MKANSI <51905275+Cleareen@users.noreply.github.com>
2024-05-17 09:24:51 +0200

    Update aggregated_lambda_function.py

--- student_solution_files/aggregated_lambda_function.py
+++ student_solution_files/aggregated_lambda_function.py
@@ -36,7 +36,25 @@ def key_phrase_finder(list_of_important_phrases, list_of_extracted_phrases):
 
 # ** Insert sentiment extraction function **
 # --- Insert your code here ---
- 
+def find_max_sentiment(Comprehend_Sentiment_Output):
+    
+    sentiment_score = 0
+
+    if Comprehend_Sentiment_Output['Sentiment'] == 'POSITIVE':
+        sentiment_score = Comprehend_Sentiment_Output['SentimentScore']['Positive']
+
+    elif Comprehend_Sentiment_Output['Sentiment'] == 'NEGATIVE':
+        sentiment_score = Comprehend_Sentiment_Output['SentimentScore']['Negative']
+
+    elif Comprehend_Sentiment_Output['Sentiment'] == 'NEUTRAL':
+        sentiment_score = Comprehend_Sentiment_Output['SentimentScore']['Neutral']
+
+    else:
+        sentiment_score = Comprehend_Sentiment_Output['SentimentScore']['Mixed']
+
+    print(sentiment_score, Comprehend_Sentiment_Output['Sentiment'])
+    
+    return Comprehend_Sentiment_Output['Sentiment'], sentiment_score
 # -----------------------------
 
 # ** Insert email responses function **
